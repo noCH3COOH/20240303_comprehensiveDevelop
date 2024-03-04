e3468806263a2703cc7aad1b0e8412f378754b82
noCH3COOH <13178669726@163.com>
2024-03-04 21:49:38 +0800

    20240304
    add：web服务相关代码，未完成
    todo：littleFs静态文件系统

--- 20240303_comprehensiveDevelop.ino
+++ 20240303_comprehensiveDevelop.ino
@@ -1,9 +1,12 @@
 void setup() {
   // put your setup code here, to run once:
     Serial.begin(115200);
+    connect_NET();
+    web_server();
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-    Serial.printf("hello world!\r\n");
+    DNS_request_loop();
 }
