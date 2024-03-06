3c61d428a65b17f5b4c5ed77b60ea3d697e7c775
noCH3COOH <13178669726@163.com>
2024-03-06 15:17:18 +0800

    20240306_1516
    add：更改工程文件结构
    todo：设计好看的web界面

---
+++ global.h
@@ -0,0 +1,10 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+#include "ESPAsyncWebServer.h"
+#include <ArduinoJson.h>
+#include <LittleFS.h>
+#include <WiFi.h>
+#include <DNSServer.h>
+
+#endif
--- net_connect.ino
+++ net_connect.ino
@@ -1,19 +1,17 @@
-/*WIFI连接*/
-#include <WiFi.h>
-#include <DNSServer.h>
+#include "global.h"
 
 DNSServer dnsserver;
 
-/***********************************************************************************
- * 函数:连接WIFI
- * ssid:        WIFI名称
- * password:    WIFI密码
- * return:      连接成功返回true
- ***********************************************************************************/
+/**
+ * @brief 连接WIFI
+ * @param ssid WIFI名称
+ * @param password WIFI密码
+ */
 void connect_WIFI(String ssid, String password)
 {
     WiFi.begin(ssid.c_str(), password.c_str()); // 连接WIFI
     Serial.print("连接WIFI");
+    
     // 循环，10秒后连接不上跳出循环
     int i = 0;
     while (WiFi.status() != WL_CONNECTED)
@@ -28,36 +26,41 @@ void connect_WIFI(String ssid, String password)
             return;
         }
     }
+    
     Serial.println();
     IPAddress local_IP = WiFi.localIP();
     Serial.print("WIFI连接成功，本地IP地址:"); // 连接成功提示
     Serial.println(local_IP);
 }
 
-/***********************************************************************************
- * 设置AP和STA共存模式，设置DNS服务器
- ***********************************************************************************/
+/**
+ * @brief 连接WIFI
+*/
 void connect_NET()
 {
     const byte DNS_PORT = 53;                         // DNS端口
     const String url = "ESPAP.com";                   // 域名
+    const char *APSsid = "esp32_AP";                  // AP SSID
+    const char *APPassword = "12345678";              // AP wifi密码
     IPAddress APIp(10, 0, 10, 1);                     // AP IP
     IPAddress APGateway(10, 0, 10, 1);                // AP网关
     IPAddress APSubnetMask(255, 255, 255, 0);         // AP子网掩码
-    const char *APSsid = "esp32_AP";                  // AP SSID
-    const char *APPassword = "12345678";              // AP wifi密码
+
     wifi_connect();                                   // 连接WIFI
+
     WiFi.mode(WIFI_AP_STA);                           // 打开AP和STA共存模式
     WiFi.softAPConfig(APIp, APGateway, APSubnetMask); // 设置AP的IP地址，网关和子网掩码
     WiFi.softAP(APSsid, APPassword, 6);               // 设置AP模式的登陆名和密码
+
     dnsserver.start(DNS_PORT, url, APIp);             // 设置DNS的端口、网址、和IP
+
     Serial.print("AP模式IP地址为:");
     Serial.println(WiFi.softAPIP());
 }
 
-/***********************************************************************************
- * DNS处理请求的循环
- ***********************************************************************************/
+/**
+ * @brief DNS服务请求处理
+*/
 void DNS_request_loop()
 {
     dnsserver.processNextRequest();
