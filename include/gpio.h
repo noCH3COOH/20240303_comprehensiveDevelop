7638147a45de6b12b4b0291420c76085dc9d4e72
noCH3COOH <13178669726@163.com>
2024-04-02 12:04:59 +0800

    20240402_1204
    update：更新 gpio 架构

--- include/gpio.h
+++ include/gpio.h
@@ -16,11 +16,11 @@
 #ifdef LED_connectVCC
     #define LED_ON LOW
     #define LED_OFF HIGH
-    #define PWM_CALC(x) (1023 - x)
+    #define PWM_CALC(x) int(1023 - x)
 #else
     #define LED_ON HIGH
     #define LED_OFF LOW
-    #define PWM_CALC(x) x
+    #define PWM_CALC(x) int(x)
 #endif
 
 #define BRIGHTNESS_CTRL_LED_CHANNEL 1
@@ -31,9 +31,13 @@
 enum GPIO_t_ErrorCode
 {
     GPIO_t_ERROR = 0,
-    GPIO_t_OK = 1,
-    GPIO_t_HIGH = 2,
-    GPIO_t_LOW = 3
+    GPIO_t_OK = 1
+};
+
+template <typename T>
+struct GPIO_t_retval {
+    T data;
+    GPIO_t_ErrorCode err;
 };
 
 // ==================== classes ====================
@@ -46,37 +50,42 @@ class GPIO_t
         uint8_t pin_mode;
 
         uint8_t pwm_channel;
-        uint32_t pwm_duty;
+        uint8_t pwm_duty;
         
         uint8_t pwm_resolution;
         uint32_t pwm_freq;
 
         bool pwm_flag;
+        bool adc_flag;
+        const uint8_t adc_bit = 12;
     
     public:
         GPIO_t();
-        GPIO_t(uint8_t pin, uint8_t pin_mode);
-        GPIO_t(uint8_t pwm_channel, uint8_t pin, uint32_t freq, uint8_t resolution, uint32_t duty);
+        GPIO_t(uint8_t pin);
         ~GPIO_t();
 
-        enum GPIO_t_ErrorCode add_pin(uint8_t pin);
-        enum GPIO_t_ErrorCode delete_pin(uint8_t pin);
+        GPIO_t_retval<bool> on();
+        GPIO_t_retval<bool> off();
         
-        void on();
-        void off();
+        GPIO_t_retval<bool> add_pin(uint8_t pin);
+        GPIO_t_retval<bool> delete_pin(uint8_t pin);
         
-        void set_pwm_channel(uint8_t pwm_channel);
-        void set_pwm_duty(uint32_t duty);
-        void set_pwm_resolution(uint8_t resolution);
-        void set_pwm_freq(uint32_t freq);
+        GPIO_t_retval<bool> set_mode(String mode = "plain", uint8_t pin_mode);
+        GPIO_t_retval<bool> set_mode(String mode = "pwm", uint8_t pwm_channel, uint32_t freq, uint8_t resolution, uint8_t duty);
+        GPIO_t_retval<bool> set_mode(String mode = "adc");
+        
+        GPIO_t_retval<bool> set_pwm_channel(uint8_t pwm_channel);
+        GPIO_t_retval<bool> set_pwm_resolution(uint8_t resolution);
+        GPIO_t_retval<bool> set_pwm_duty(uint8_t duty);
+        GPIO_t_retval<bool> set_pwm_freq(uint32_t freq);
 
-        enum GPIO_t_ErrorCode get_pinState(uint8_t pin);
-        
-        uint8_t get_pin(uint8_t index);
-        uint8_t get_pwm_channel();
-        uint32_t get_pwm_duty();
-        uint8_t get_pwm_resolution();
-        uint32_t get_pwm_freq();
+        GPIO_t_retval<uint8_t> get_pinState(uint8_t pin);
+        GPIO_t_retval<uint8_t> get_pin(uint8_t index);
+        GPIO_t_retval<uint8_t> get_pwm_channel();
+        GPIO_t_retval<uint8_t> get_pwm_resolution();
+        GPIO_t_retval<uint8_t> get_pwm_duty();
+        GPIO_t_retval<uint32_t> get_pwm_freq();
+        GPIO_t_retval<float> get_adc_value(uint8_t pin);
 };
 
 // ==================== global variables ====================
--- src/gpio.cpp
+++ src/gpio.cpp
@@ -2,7 +2,7 @@
 
 // ==================== global variables ====================
 
-GPIO_t led(BRIGHTNESS_CTRL_LED_CHANNEL, BRIGHTNESS_CTRL_LED_PIN, 5000, 10, 50);
+GPIO_t led(BRIGHTNESS_CTRL_LED_PIN);
 
 int loop_duty = 0;
 bool loop_direation = true;
@@ -14,6 +14,7 @@ bool loop_direation = true;
 */
 void LED_init()
 {
+    led.set_mode("pwm", BRIGHTNESS_CTRL_LED_CHANNEL, 5000, 10, 50);
     led.on();
 }
 
@@ -52,7 +53,7 @@ void LED_root()
 */
 void getCallback_readLED(AsyncWebServerRequest *request)
 {
-    const String str_request = String(std::to_string(PWM_CALC(led.get_pwm_duty()) / 10.23).c_str()) + "%";
+    const String str_request = String(std::to_string(led.get_pwm_duty().data).c_str()) + "%";
     request->send(200, "text/plain", str_request);
 }
 
@@ -68,7 +69,7 @@ void postCallback_setLED(AsyncWebServerRequest *request)
 
     for(int i=0; i<8; i++)
     {
-        led.set_pwm_duty(PWM_CALC(LED_duty * 10.23));
+        led.set_pwm_duty(LED_duty);
         delayNoBlock_ms(15);
     }
     delayNoBlock_ms(500);
@@ -99,13 +100,12 @@ GPIO_t::GPIO_t()
 /**
  * @brief GPIO 构造函数
  * @param pin 引脚
- * @param pin_mode 引脚模式 INPUT OUTPUT PULLUP INPUT_PULLUP PULLDOWN INPUT_PULLDOWN OPEN_DRAIN OUTPUT_OPEN_DRAIN ANALOG
 */
-GPIO_t::GPIO_t(uint8_t pin, uint8_t pin_mode)
+GPIO_t::GPIO_t(uint8_t pin)
 {
     this->pin[0] = pin;
     this->pin_count = 1;
-    this->pin_mode = OUTPUT;
+    this->pin_mode = 0;
 
     this->pwm_channel = 0;
     this->pwm_duty = 0;
@@ -113,21 +113,7 @@ GPIO_t::GPIO_t(uint8_t pin, uint8_t pin_mode)
     this->pwm_freq = 0;
 
     this->pwm_flag = false;
-}
-
-GPIO_t::GPIO_t(uint8_t pwm_channel, uint8_t pin, uint32_t freq, uint8_t resolution, uint32_t duty)
-{
-    this->pin[0] = pin;
-    this->pin_count = 1;
-    this->pin_mode = OUTPUT;
-
-
-    this->pwm_channel = pwm_channel;
-    this->pwm_freq = freq;
-    this->pwm_resolution = resolution;
-    this->pwm_duty = duty;
-
-    this->pwm_flag = true;
+    this->adc_flag = false;
 }
 
 /**
@@ -139,18 +125,62 @@ GPIO_t::~GPIO_t()
 }
 
 /**
+ * @brief 打开引脚
+*/
+GPIO_t_retval<bool> GPIO_t::on()
+{
+    if (this->pwm_flag)
+    {
+        ledcSetup(this->pwm_channel, this->pwm_freq, this->pwm_resolution);
+        for(int i=0; i<(this->pin_count); i++)
+            ledcAttachPin(this->pin[i], this->pwm_channel);
+        ledcWrite(this->pwm_channel, PWM_CALC(this->pwm_duty * 10.23));
+    }
+    else if(this->adc_flag)
+    {
+        for(int i=0; i<(this->pin_count); i++)
+            pinMode(this->pin[i], INPUT);
+    }
+    else
+    {
+        for(int i=0; i<(this->pin_count); i++)
+            digitalWrite(this->pin[0], LED_ON);
+    }
+
+    return {true, GPIO_t_OK};
+}
+
+/**
+ * @brief 关闭引脚
+*/
+GPIO_t_retval<bool> GPIO_t::off()
+{
+    if (this->pwm_flag)
+    {
+        ledcWrite(this->pwm_channel, 0);
+    }
+    else
+    {
+        for(int i=0; i<(this->pin_count); i++)
+            digitalWrite(this->pin[i], LED_OFF);
+    }
+
+    return {true, GPIO_t_OK};
+}
+
+/**
  * @brief 添加引脚
  * @param pin 引脚
  * @return 是否添加成功
  * @note GPIO_t_OK 添加成功
  * @note GPIO_t_ERROR 添加失败，可能有重复引脚
 */
-enum GPIO_t_ErrorCode GPIO_t::add_pin(uint8_t pin)
+GPIO_t_retval<bool> GPIO_t::add_pin(uint8_t pin)
 {
-    for(int i=0; i<this->pin_count; i++)
+    for(int i=0; i<(this->pin_count); i++)
     {
         if (this->pin[i] == pin)
-            return GPIO_t_ERROR;
+            return {false, GPIO_t_ERROR};
     }
     
     this->pin[this->pin_count] = pin;
@@ -160,12 +190,12 @@ enum GPIO_t_ErrorCode GPIO_t::add_pin(uint8_t pin)
     {
         ledcSetup(this->pwm_channel, this->pwm_freq, this->pwm_resolution);
         ledcAttachPin(pin, this->pwm_channel);
-        ledcWrite(this->pwm_channel, this->pwm_duty);
+        ledcWrite(this->pwm_channel, PWM_CALC(this->pwm_duty * 10.23));
     }
     else
         pinMode(pin, this->pin_mode);
 
-    return GPIO_t_OK;
+    return {true, GPIO_t_OK};
 }
 
 /**
@@ -175,95 +205,112 @@ enum GPIO_t_ErrorCode GPIO_t::add_pin(uint8_t pin)
  * @note GPIO_t_OK 删除成功
  * @note GPIO_t_ERROR 删除失败
 */
-enum GPIO_t_ErrorCode GPIO_t::delete_pin(uint8_t pin)
+GPIO_t_retval<bool> GPIO_t::delete_pin(uint8_t pin)
 {
-    for(int i=0; i<this->pin_count; i++)
+    for(int i=0; i<(this->pin_count); i++)
     {
         if (this->pin[i] == pin)
         {
             for(int j=i; j<this->pin_count-1; j++)
                 this->pin[j] = this->pin[j+1];
             this->pin_count--;
-            return GPIO_t_OK;
+
+            if(this->pwm_flag)
+                ledcDetachPin(pin);
+
+            return {true, GPIO_t_OK};
         }
     }
-    return GPIO_t_ERROR;
+    return {false, GPIO_t_ERROR};
 }
 
 /**
- * @brief 打开引脚
+ * @brief 设置模式
 */
-void GPIO_t::on()
+GPIO_t_retval<bool> GPIO_t::set_mode(String mode = "plain", uint8_t pin_mode)
 {
-    if (this->pwm_flag)
-    {
-        ledcSetup(this->pwm_channel, this->pwm_freq, this->pwm_resolution);
-        for(int i=0; i<this->pin_count; i++)
-            ledcAttachPin(this->pin[i], this->pwm_channel);
-        ledcWrite(this->pwm_channel, this->pwm_duty);
-    }
-    else
-    {
-        for(int i=0; i<this->pin_count; i++)
-            digitalWrite(this->pin[0], LED_ON);
-    }
+    this->pin_mode = pin_mode;
+
+    for(int i=0; i<(this->pin_count); i++)
+        pinMode(this->pin[i], pin_mode);
+    
+    return {true, GPIO_t_OK};
 }
 
-/**
- * @brief 关闭引脚
-*/
-void GPIO_t::off()
+GPIO_t_retval<bool> GPIO_t::set_mode(String mode = "pwm", uint8_t pwm_channel, uint32_t freq, uint8_t resolution, uint8_t duty)
 {
-    if (this->pwm_flag)
-    {
-        ledcWrite(this->pwm_channel, 0);
-    }
-    else
-    {
-        for(int i=0; i<this->pin_count; i++)
-            digitalWrite(this->pin[i], LED_OFF);
-    }
+    this->pwm_channel = pwm_channel;
+    this->pwm_freq = freq;
+    this->pwm_resolution = resolution;
+    this->pwm_duty = duty;
+
+    ledcSetup(this->pwm_channel, this->pwm_freq, this->pwm_resolution);
+    
+    for(int i=0; i<(this->pin_count); i++)
+        ledcAttachPin(this->pin[i], this->pwm_channel);
+    
+    ledcWrite(this->pwm_channel, PWM_CALC(this->pwm_duty * 10.23));
+
+    return {true, GPIO_t_OK};
 }
+
+GPIO_t_retval<bool> GPIO_t::set_mode(String mode = "adc")
+{
+    for(int i=0; i<(this->pin_count); i++)
+        pinMode(this->pin[i], INPUT);
+    
+    return {true, GPIO_t_OK};
+}
+
 
 /**
  * @brief 设置 PWM 通道
  * @param pwm_channel PWM 通道
 */
-
-void GPIO_t::set_pwm_channel(uint8_t pwm_channel)
+GPIO_t_retval<bool> GPIO_t::set_pwm_channel(uint8_t pwm_channel)
 {   
     this->pwm_channel = pwm_channel;
+
     ledcWrite(pwm_channel, 0);
-    for(int i=0; i<this->pin_count; i++)
+    
+    for(int i=0; i<(this->pin_count); i++)
         ledcDetachPin(this->pin[i]);
 
     ledcSetup(pwm_channel, this->pwm_freq, this->pwm_resolution);
-    for(int i=0; i<this->pin_count; i++)
+    
+    for(int i=0; i<(this->pin_count); i++)
         ledcAttachPin(this->pin[i], pwm_channel);
-    ledcWrite(pwm_channel, this->pwm_duty);
+    
+    ledcWrite(pwm_channel, PWM_CALC(this->pwm_duty * 10.23));
+
+    return {true, GPIO_t_OK};
 }
 
 /**
  * @brief 设置 PWM 占空比
  * @param duty 占空比
 */
-void GPIO_t::set_pwm_duty(uint32_t duty)
+GPIO_t_retval<bool> GPIO_t::set_pwm_duty(uint8_t duty)
 {
     this->pwm_duty = duty;
 
-    ledcWrite(this->pwm_channel, duty);
+    ledcWrite(this->pwm_channel, PWM_CALC(duty * 10.23));
+
+    return {true, GPIO_t_OK};
 }
 
 /**
  * @brief 设置 PWM 分辨率
  * @param resolution 分辨率
 */
-void GPIO_t::set_pwm_resolution(uint8_t resolution)
+GPIO_t_retval<bool> GPIO_t::set_pwm_resolution(uint8_t resolution)
 {
     this->pwm_resolution = resolution;
 
     ledcSetup(this->pwm_channel, this->pwm_freq, resolution);
-    ledcWrite(this->pwm_channel, this->pwm_duty);
+    ledcWrite(this->pwm_channel, PWM_CALC(this->pwm_duty * 10.23));
+
+    return {true, GPIO_t_OK};
 }
 
 /**
@@ -271,26 +318,27 @@ void GPIO_t::set_pwm_resolution(uint8_t resolution)
  * @param freq 频率
 */
 
-void GPIO_t::set_pwm_freq(uint32_t freq)
+GPIO_t_retval<bool> GPIO_t::set_pwm_freq(uint32_t freq)
 {
     this->pwm_freq = freq;
 
     ledcSetup(this->pwm_channel, freq, this->pwm_resolution);
-    ledcWrite(this->pwm_channel, this->pwm_duty);
+    ledcWrite(this->pwm_channel, PWM_CALC(this->pwm_duty * 10.23));
+
+    return {true, GPIO_t_OK};
 }
 
 /**
  * @brief 获取引脚状态
  * @param pin 引脚 
  * @return 引脚状态
- * @note GPIO_t_OK PWM 通道占空比大于 0
- * @note GPIO_t_HIGH 引脚高电平
- * @note GPIO_t_LOW 引脚低电平
- * @note GPIO_t_ERROR 引脚错误
+ * @note {占空比, GPIO_t_OK} PWM
+ * @note {1/0, GPIO_t_OK} plain引脚
+ * @note {-1, GPIO_t_ERROR} 失败
 */
-enum GPIO_t_ErrorCode GPIO_t::get_pinState(uint8_t pin)
+GPIO_t_retval<uint8_t> GPIO_t::get_pinState(uint8_t pin)
 {
-    for(int i=0; i<this->pin_count; i++)
+    for(int i=0; i<(this->pin_count); i++)
     {
         if(pin != this->pin[i])
             continue;
@@ -298,22 +346,23 @@ enum GPIO_t_ErrorCode GPIO_t::get_pinState(uint8_t pin)
         {
             if (this->pwm_flag)
             {
-                if (ledcRead(this->pwm_channel) > 0)
-                    return GPIO_t_OK;
+                uint8_t read_duty = PWM_CALC(ledcRead(this->pwm_channel)) / 10.23;
+                if (read_duty > 0)
+                    return {read_duty, GPIO_t_OK};
                 else
-                    return GPIO_t_LOW;
+                    return {0, GPIO_t_OK};
             }
             else
             {
                 if(LED_ON == digitalRead(pin))
-                    return GPIO_t_HIGH;
+                    return {1, GPIO_t_OK};
                 else
-                    return GPIO_t_LOW;
+                    return {0, GPIO_t_OK};
             }
         }
     }
 
-    return GPIO_t_ERROR;
+    return {-1, GPIO_t_ERROR};
 }
 
 /**
@@ -321,44 +370,45 @@ enum GPIO_t_ErrorCode GPIO_t::get_pinState(uint8_t pin)
  * @param index 索引
  * @return 引脚
 */
-uint8_t GPIO_t::get_pin(uint8_t index)
+GPIO_t_retval<uint8_t> GPIO_t::get_pin(uint8_t index)
 {
-    return this->pin[index];
-}
-
-/**
- * @brief 获取 PWM 通道
- * @return PWM 通道
-*/
-uint8_t GPIO_t::get_pwm_channel()
-{
-    return this->pwm_channel;
-}
-
-/**
- * @brief 获取 PWM 占空比
- * @return PWM 占空比
-*/
-uint32_t GPIO_t::get_pwm_duty()
-{
-    return this->pwm_duty;
+    return {this->pin[index], GPIO_t_OK};
 }
 
 /**
  * @brief 获取 PWM 分辨率
  * @return PWM 分辨率
 */
-uint8_t GPIO_t::get_pwm_resolution()
+GPIO_t_retval<uint8_t> GPIO_t::get_pwm_resolution()
 {
-    return this->pwm_resolution;
+    return {this->pwm_resolution, GPIO_t_OK};
+}
+
+/**
+ * @brief 获取 PWM 通道
+ * @return PWM 通道
+*/
+GPIO_t_retval<uint8_t> GPIO_t::get_pwm_channel()
+{
+    return {this->pwm_channel, GPIO_t_OK};
+}
+
+/**
+ * @brief 获取 PWM 占空比
+ * @return PWM 占空比
+*/
+GPIO_t_retval<uint8_t> GPIO_t::get_pwm_duty()
+{
+    return {this->pwm_duty, GPIO_t_OK};
 }
 
 /**
  * @brief 获取 PWM 频率
  * @return PWM 频率
 */
-uint32_t GPIO_t::get_pwm_freq()
+GPIO_t_retval<uint32_t> GPIO_t::get_pwm_freq()
 {
     this->pwm_freq = ledcReadFreq(this->pwm_channel);
-    return this->pwm_freq;
+
+    return {this->pwm_freq, GPIO_t_OK};
 }
